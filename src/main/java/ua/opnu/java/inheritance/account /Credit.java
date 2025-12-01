077ca8548aca8a98fdd0d30a88abf1cbd86198a9
vladaguniya <9100299@stud.op.edu.ua>
2025-12-02 00:31:16 +0200

    Create Credit.java

---
+++ src/main/java/ua/opnu/java/inheritance/account /Credit.java
@@ -0,0 +1,13 @@
+package ua.opnu.java.inheritance.account;
+
+public class Credit {
+    private int balance;
+
+    public Credit(int balance) {
+        this.balance = balance;
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+}
