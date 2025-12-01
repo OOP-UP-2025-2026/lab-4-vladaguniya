ea050e41d5069e71efc71c4f62572de15fc851c6
vladaguniya <9100299@stud.op.edu.ua>
2025-12-02 00:33:28 +0200

    Create Employee.java

---
+++ src/main/java/ua/opnu/java/inheritance/bill /Employee.java
@@ -0,0 +1,13 @@
+package ua.opnu.java.inheritance.bill;
+
+public class Employee {
+    private String name;
+
+    public Employee(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
