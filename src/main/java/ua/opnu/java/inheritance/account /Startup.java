8ae0e115263f8803a7dc8a1d95b4cbbc44a61596
vladaguniya <9100299@stud.op.edu.ua>
2025-12-02 00:32:18 +0200

    Create Startup.java

---
+++ src/main/java/ua/opnu/java/inheritance/account /Startup.java
@@ -0,0 +1,13 @@
+package ua.opnu.java.inheritance.account;
+
+public class Startup {
+    private int balance;
+
+    public Startup(int balance) {
+        this.balance = balance;
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+}
